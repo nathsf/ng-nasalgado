538ce36f6ab9f960d350ac88a8f348b7267620ab
Nathalysf <nathalysalgado.f@gmail.com>
2022-06-29 05:28:01 +0200

    directivas y pipe

--- src/app/app-content/inicio/inicio.component.ts
+++ src/app/app-content/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  show:boolean = false;
+  titledefault:string = "";
+  LISTA = [
+    {id: 1, name:'Alumno 1'},
+    {id: 2, name:'Alumno 2'},
+    {id: 5, name:'Alumno 3'},
+    {id: 3, name:'Alumno 4'},
+    {id: 4, name:'Alumno 5'}
+  ];
 
+  lista = this.LISTA;
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  Saludar(){
+    this.show = true;
+    this.titledefault = "Hola Alumno!"
+
+  }
+  Salir(){
+    this.titledefault = "Bye alumno!"
+    this.show = false;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { AppContentComponent } from './app-content/app-content.component';
+import { InicioComponent } from './app-content/inicio/inicio.component';
 
 @NgModule({
   declarations: [			
     AppComponent,
       AppToolbarComponent,
       AppNavbarComponent,
-      AppContentComponent
+      AppContentComponent,
+      InicioComponent
    ],
   imports: [
     BrowserModule
